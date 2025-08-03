ec4ebc0168223bd24383f7fc8f715faf14810f4c
ivan <125134048+rahadianivan09@users.noreply.github.com>
2025-08-04 01:50:51 +0700

    Delete fix-all-option-role.sh

--- fix-all-option-role.sh
+++
@@ -1,9 +0,0 @@
-grep -rl --include=\*.tsx '<option value=Role.' app actions | while read file; do
-  echo "í´§ Memperbaiki $file"
-  # Tambah { setelah value=
-  sed -i 's/<option value=Role\./<option value={Role./g' "$file"
-  # Tambah } sebelum >
-  sed -i 's/Role\.\([A-Z_]\+\)>/Role.\1}>/g' "$file"
-done
-
-echo "âœ… Semua <option value=Role...> sudah dibungkus {}"
